3891dee33dac8f934d84afebdc4ad95dc7ceff6b
vishalk17 <vishalkapadi95@gmail.com>
2017-12-02 11:07:32 -0800

    board uses prebuilt kernel image

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,12 +1,6 @@
-KERNEL_DEFCONFIG := x500_defconfig
-KERNEL_DIR := kernel/leeco/x3
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+LOCAL_PATH := $(call my-dir)
 
-include $(TOP)/$(KERNEL_DIR)/Android.mk
-
-# device.mk doesn't know about us, and we can't PRODUCT_COPY_FILES here.
-# So cp will do.
-.PHONY: $(PRODUCT_OUT)/kernel
-$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
-	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
+ifneq ($(filter x3,$(TARGET_DEVICE)),)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
+# include the non-open-source counterpart to this file
