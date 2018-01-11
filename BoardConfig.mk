1de954c8466aad5e39336fc84ccdec219d40b1e4
vishalk17 <vishalkapadi95@gmail.com>
2018-01-11 14:48:58 +0000

    removed manifest.xml,Compatibility_matrix.xml,manifest_boot.xml
    getting build errors, so i have removed this shit.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -265,10 +265,6 @@ endif
 #net.tethering.noprovisioning=true \
 #ro.setupwizard.rotation_locked=true
 
-DEVICE_MATRIX_FILE := $(LOCAL_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
-DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest_boot.xml
-
 #TARGET_BOARD_KERNEL_HEADERS := \
 #	   device/leeco/x3/headers/common/kernel-headers \
 #	   device/leeco/x3/headers/mt6795/kernel-headers
