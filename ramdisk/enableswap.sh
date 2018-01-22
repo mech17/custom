b364f5ed6b8c0043eeb1c5e0d2bdc8713e8592ca
vishalk17 <vishalkapadi95@gmail.com>
2018-01-22 18:00:06 +0000

    revert back to previous paths

--- ramdisk/enableswap.sh
+++ ramdisk/enableswap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo 536870912 > /sys/block/zram0/disksize
-/system/vendor/bin/tiny_mkswap /dev/block/zram0
-/system/vendor/bin/tiny_swapon /dev/block/zram0
+/system/bin/tiny_mkswap /dev/block/zram0
+/system/bin/tiny_swapon /dev/block/zram0
