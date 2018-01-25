0a62a8dd8ccb82cf711735d922465ce06fd41027
vishalk17 <vishalkapadi95@gmail.com>
2018-01-25 18:45:16 +0000

    added HIDLs & vendor manifest

--- device.mk
+++ device.mk
@@ -90,6 +90,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
+	android.hardware.wifi@1.0-service \
 	lib_driver_cmd_mt66xx \
 	libwpa_client \
 	hostapd \
@@ -306,3 +307,90 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy \
     $(DEVICE_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec-seccomp.policy
+
+#Manifest
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/manifest.xml:system/vendor/manifest.xml
+
+# GPS force mode
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.force.gps.mode=gnss
+
+#Audio HIDL
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl
+#    android.hardware.soundtrigger@2.0-impl \
+
+# Power HAL
+PRODUCT_PACKAGES += \ðŸŽðŸŽ
+    android.hardware.power@1.0-impl
+
+#gralloc/graphics HAL/HW Composer
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.renderscript@1.0-impl \
+    android.hardware.memtrack@1.0-imp
+
+#memtrack
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
+# Camera HAL
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service
+
+# Bluetooth HAL
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
+
+#GNSS HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
+#USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+# Omx
+PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0
+
+# Keymaster HIDL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+#    android.hardware.keymaster@3.0-service
+
+# Gatekeeper HIDL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
+#    android.hardware.gatekeeper@1.0-service
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+
+# Drm
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
