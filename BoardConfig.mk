3d25c586d83bf4b8ddfbf241f87e1feb654acf58
vishalk17 <vishalkapadi95@gmail.com>
2018-01-26 03:13:27 +0000

    use fstab.mt6795 as a recovery fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,7 +163,7 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 ifeq ($(WITH_TWRP),true)
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/ramdisk/twrp.fstab
 else
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/ramdisk/recovery.fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/ramdisk/fstab.mt6795
 endif
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
 BOARD_HAS_NO_SELECT_BUTTON := true
