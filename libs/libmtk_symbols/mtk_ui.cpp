faf7fce29de2fbb543961c49021da2f14dc5f3b8
joshi <joshi.naga3@gmail.com>
2018-02-15 19:15:27 +0530

    add graphic buffer symbol to shim

--- libs/libmtk_symbols/mtk_ui.cpp
+++ libs/libmtk_symbols/mtk_ui.cpp
@@ -81,4 +81,13 @@ extern "C" {
     void _ZN7android5Fence4waitEj(unsigned int timeout) {
         _ZN7android5Fence4waitEi(static_cast<int>(timeout));
     }
+extern "C" void _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEEb(
+	void* outProducer, void* outConsumer, bool consumerIsSurfaceFlinger);
+
+extern "C" void _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEERKNS1_INS_19IGraphicBufferAllocEEE(
+	void* outProducer, void* outConsumer, void* allocator __unused, bool consumerIsSurfaceFlinger)
+{
+    _ZN7android11BufferQueue17createBufferQueueEPNS_2spINS_22IGraphicBufferProducerEEEPNS1_INS_22IGraphicBufferConsumerEEEb(
+	outProducer, outConsumer, consumerIsSurfaceFlinger);
+}
 }
