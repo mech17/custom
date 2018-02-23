126a06aec749cc0cc6636585cb2e1773b2788d07
vishalk95 <32028791+vishalk95@users.noreply.github.com>
2018-02-23 07:47:26 -0800

    device.mk: uncomment dex2oat

--- device.mk
+++ device.mk
@@ -56,13 +56,13 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.debug.xlog.enable=0 \
 	camera.disable_zsl_mode=1
 
-ifeq (lineage_x3,$(TARGET_PRODUCT))	#this is included only in lineage atm as some other roms have issue with this
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	dalvik.vm.dex2oat-Xms=64m \
-	dalvik.vm.dex2oat-Xmx=64m \
-	dalvik.vm.image-dex2oat-Xms=64m \
-	dalvik.vm.image-dex2oat-Xmx=512m
-endif
+#ifeq (lineage_x3,$(TARGET_PRODUCT))	#this is included only in lineage atm as some other roms have issue with this
+#PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+#	dalvik.vm.dex2oat-Xms=64m \
+#	dalvik.vm.dex2oat-Xmx=64m \
+#	dalvik.vm.image-dex2oat-Xms=64m \
+#	dalvik.vm.image-dex2oat-Xmx=512m
+#endif
 
 # PRODUCT_PROPERTY_OVERRIDES
 PRODUCT_PROPERTY_OVERRIDES += \
