b8c758f6648ba8c854b7248393dcce217f4a9e04
fire855 <thefire855@gmail.com>
2018-03-26 11:10:23 +0000

    build libbt-vendor

--- device.mk
+++ device.mk
@@ -359,7 +359,8 @@ PRODUCT_PACKAGES += \
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor
 
 #GNSS HAL
 PRODUCT_PACKAGES += \
